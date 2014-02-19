3822cb9e67a549c13fc9acdb2443134cc2e41c60
Seiei Higa <hanachin@gmail.com>
2014-02-19 23:26:42 +0900

    emacs.d自体まるごとリンクはる

--- symlink.sh
+++ symlink.sh
@@ -4,8 +4,5 @@ for dotfile in $dotfiles/*; do
   echo "ln -Fis $dotfile $HOME/.${dotfile##*/}"
   ln -Fis $dotfile $HOME/.${dotfile##*/}
 done
-if [ ! -d $HOME/.emacs.d ]; then
-    mkdir $HOME/.emacs.d/
-fi
-echo "ln -Fis $dotfiles/emacs.d/init.el $HOME/.emacs.d/init.el"
-ln -Fis $dotfiles/emacs.d/init.el $HOME/.emacs.d/init.el
+echo "ln -Fis $dotfiles/emacs.d/ $HOME/.emacs.d"
+ln -Fis $dotfiles/emacs.d/ $HOME/.emacs.d
