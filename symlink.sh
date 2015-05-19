84f25acb40525569085c48e0f9b178aa366873cb
Seiei Miyagi <hanachin@gmail.com>
2015-05-19 16:20:12 +0900

    Remove create symlink shell script

--- symlink.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-dotfiles=$(cd $(dirname $0) && pwd)
-for dotfile in $dotfiles/*; do
-  echo "ln -Fis $dotfile $HOME/.${dotfile##*/}"
-  ln -Fis $dotfile $HOME/.${dotfile##*/}
-done
-echo "ln -Fis $dotfiles/emacs.d/ $HOME/.emacs.d"
-ln -Fis $dotfiles/emacs.d/ $HOME/.emacs.d
