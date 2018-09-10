1a8430bb67dd85a9f39f25b351bc8e933bba145f
Seiei Miyagi <hanachin@gmail.com>
2018-09-10 09:59:35 +0900

    emacs.d/init.el: Remove custom-set-variables

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -139,17 +139,3 @@
 
 
 (setq x-alt-keysym 'meta)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (inf-ruby smartrep ruby-end popwin multiple-cursors markdown-mode magit e2wm color-theme-solarized auto-install auto-complete anything))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
