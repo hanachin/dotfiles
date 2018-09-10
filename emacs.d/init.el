44c2e07443a7d77b6d889cfd0b6db6594328c949
Seiei Miyagi <hanachin@gmail.com>
2018-09-10 09:28:51 +0900

    emacs.d/init.el: Rename namespace my- -> my/

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -66,7 +66,7 @@
 
 ;; Meadow/Emacs memo: ウィンドウ/バッファの操作
 ;; http://www.bookshelf.jp/soft/meadow_29.html
-(defun my-make-scratch (&optional arg)
+(defun my/make-scratch (&optional arg)
   (interactive)
   (progn
     ;; "*scratch*" を作成して buffer-list に放り込む
@@ -80,21 +80,21 @@
     (cond ((= arg 0) (message "*scratch* is cleared up."))
           ((= arg 1) (message "another *scratch* is created")))))
 
-(defun my-buffer-name-list ()
+(defun my/buffer-name-list ()
   (mapcar (function buffer-name) (buffer-list)))
 
 (add-hook 'kill-buffer-query-functions
           ;; *scratch* バッファで kill-buffer したら内容を消去するだけにする
           (function (lambda ()
                       (if (string= "*scratch*" (buffer-name))
-                          (progn (my-make-scratch 0) nil)
+                          (progn (my/make-scratch 0) nil)
                         t))))
 
 (add-hook 'after-save-hook
           ;; *scratch* バッファの内容を保存したら *scratch* バッファを新しく作る
           (function (lambda ()
-                      (unless (member "*scratch*" (my-buffer-name-list))
-                        (my-make-scratch 1)))))
+                      (unless (member "*scratch*" (my/buffer-name-list))
+                        (my/make-scratch 1)))))
 
 ;; Emacsの基本設定: 麦汁三昧
 ;; http://mugijiru.seesaa.net/article/203900890.html#
